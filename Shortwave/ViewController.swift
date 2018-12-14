7c2485a4d21750f81b46ac0b97522c99bc70571a
Jenya Ivanova <jenyaivanova521@gmail.com>
2018-12-14 01:20:50 -0800

    Initial Commit

---
+++ Shortwave/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shortwave
+//
+//  Created by Mobile World on 12/14/18.
+//  Copyright © 2018 Mobile World. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
